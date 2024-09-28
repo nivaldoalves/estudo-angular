2e5af067a4bf60698adc3753d4bbba3f4c57707d
Nivaldo Alves <104028573+nivaldoalves@users.noreply.github.com>
2024-09-28 09:39:49 -0300

    ajust css footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatMenuModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  anoAtual = new Date().getFullYear();
 }
